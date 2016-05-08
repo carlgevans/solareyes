25a707c78a379207a73b4e5d38906d0af6036256
carlgevans <carlgevans@evfam.net>
2016-05-08 18:16:59 +0100

    Completed delete_orphaned_tests() and create_tests()

--- solareyes.py
+++ solareyes.py
@@ -41,15 +41,28 @@ class SolarEyes(object):
         te_api: A ThousandEyes API instance.
         sw_api: A SolarWinds API instance.
         settings: A SolarEyesSettings instance.
+        logger: A logging instance from the Python logging library.
     """
-    def __init__(self, te_api=None, sw_api=None, settings=None):
-        if te_api is None or sw_api is None or settings is None:
-            raise errors.Error("[%s.%s] - You must provide instances of SolarEyesSettings,"
+    def __init__(self, te_api=None, sw_api=None, settings=None, logger=None):
+        if te_api is None or sw_api is None or settings is None or logger is None:
+            raise errors.Error("[%s.%s] - You must provide instances of a Logger, SolarEyesSettings,"
                                "ThousandEyes & SolarWinds APIs." % (__name__, self.__class__.__name__))
         else:
             self.te_api = te_api
             self.sw_api = sw_api
             self.settings = settings
+            self.logger = logger
+
+    def check_apis(self):
+        """Check the availability of both SolarWinds and ThousandEyes APIs.
+
+        Returns:
+            A boolean of True if both APIs are ready, or False if an error occurred.
+        """
+        if self.te_api.status() and self.sw_api.status():
+            return True
+        else:
+            return False
 
     def get_agent_ids(self, agent_list):
         """Get a list of enterprise agent ids.
@@ -99,24 +112,15 @@ class SolarEyes(object):
         nodes = self.sw_api.query("SELECT NodeName, IPAddress FROM Orion.Nodes WHERE "
                                   "Nodes.CustomProperties.%s = TRUE" % self.settings.sw_custom_bool)
 
+        node_dict = {}
+
         for node in nodes['results']:
-            print(node['NodeName'])
-            print(node['IPAddress'])
-            print("---------------")
+            node_dict[node['IPAddress']] = node['NodeName']
 
-    def check_apis(self):
-        """Check the availability of both SolarWinds and ThousandEyes APIs.
-
-        Returns:
-            A boolean of True if both APIs are ready, or False if an error occurred.
-        """
-        if self.te_api.status() and self.sw_api.status():
-            return True
-        else:
-            return False
+        return node_dict
 
     def create_test(self, test_name=None, test_server=None):
-        """Create a test with defaults taken from settings.ini and from passed values.
+        """Create a single test with defaults taken from settings.ini and from passed values.
 
         Args:
             test_name: The name of the test as displayed in the ThousandEyes web interface.
@@ -141,13 +145,66 @@ class SolarEyes(object):
             test.port = int(self.settings.te_test_port)
             test.interval = int(self.settings.te_test_interval)
 
-        for agent_id in self.get_agent_ids(self.te_api.get_agents()):
-            test.agents.append({"agentId": agent_id})
+            for agent_id in self.get_agent_ids(self.te_api.get_agents()):
+                test.agents.append({"agentId": agent_id})
 
-        if self.te_api.create_network_test(test):
-            return True
-        else:
-            return False
+            if self.te_api.create_network_test(test):
+                return True
+            else:
+                return False
+
+    def create_tests(self, path_nodes, se_tests):
+        """Create any new tests from a passed list of SolarWinds nodes and current ThousandEyes tests.
+
+        Args:
+            path_nodes: A dictionary containing the current list of IP addresses that require path data.
+            se_tests: A current list of ThousandEyes tests that were created with SolarEyes.
+
+        Returns:
+            A boolean of True if the process was successful or False if an error occurred.
+        """
+        ignore_list = []
+
+        for test in se_tests:
+            if test.server in path_nodes:
+                    ignore_list.append(test.server)
+
+        for ip, name in path_nodes.items():
+            if ip not in ignore_list:
+                self.logger.debug("[%s] - Attempting to create test %s (%s) on ThousandEyes."
+                                  % (self.__class__.__name__, name, ip))
+
+                if self.create_test(name, ip):
+                    self.logger.info("[%s] - ThousandEyes test %s (%s) was created."
+                                     % (self.__class__.__name__, name, ip))
+                else:
+                    self.logger.error("[%s] - An error occurred while attempting to create ThousandEyes test %s (%s)."
+                                      % (self.__class__.__name__, name, ip))
+            else:
+                self.logger.debug("[%s] - %s (%s) was already present on ThousandEyes."
+                                  % (self.__class__.__name__, name, ip))
+
+    def delete_orphaned_tests(self, path_nodes, se_tests):
+        """Delete tests on ThousandEyes that don't have a matching node in SolarWinds.
+
+        Args:
+            path_nodes: A dictionary containing the current list of IP addresses that require path data.
+            se_tests: A current list of ThousandEyes tests that were created with SolarEyes.
+
+        Returns:
+            A boolean of True if the deletion was successful or False if an error occurred.
+        """
+        for se_test in se_tests:
+            if se_test.server not in path_nodes:
+                if self.te_api.delete_network_test(se_test.id):
+                    self.logger.info("[%s] - ThousandEyes test %s was deleted."
+                                     % (self.__class__.__name__, se_test.name))
+                else:
+                    self.logger.error("[%s] - An error occurred while attempting to delete ThousandEyes test %s."
+                                      % (self.__class__.__name__, se_test.name))
+            else:
+                self.logger.debug("[%s] - Thousand Eyes test %s (%s) is still present as a node in SolarWinds. "
+                                  "Deletion not required." % (self.__class__.__name__, se_test.name, se_test.server))
 
     def sync(self):
         """Perform a synchronisation between SolarWinds and ThousandEyes.
@@ -156,20 +213,20 @@ class SolarEyes(object):
             A boolean of True if the synchronisation was successful, or False if an error occurred.
         """
         if self.check_apis():
-            # Synchronise between SolarWinds and ThousandEyes.
+            # Get current lists from both SolarWinds & ThousandEyes API.
+            se_tests = self.get_se_tests(self.te_api.get_network_tests())
+            path_nodes = self.get_path_nodes()
 
-            #se_tests = self.get_se_tests(self.te_api.get_network_tests())
+            # Delete any tests on ThousandEyes that don't have a node on SolarWinds.
+            self.delete_orphaned_tests(path_nodes, se_tests)
 
-            # for se_test in se_tests:
-            #     print(se_test.)
-
-            #if self.create_test("Test", "8.8.8.8"):
-                #Log test creation here. Need to pass in logger.
-            self.get_path_nodes()
+            # Create any tests for new nodes on SolarWinds.
+            self.create_tests(path_nodes, se_tests)
 
             return True
         else:
             return False
+
 
 class SolarEyesSettings(object):
     """All external settings required by the SolarEyes class.
@@ -222,7 +279,7 @@ def main():
                                         settings['te_test_prefix'])
 
         # Pass the above API and settings instances to the constructor of SolarEyes.
-        solareyes = SolarEyes(te_api, sw_api, se_settings)
+        solareyes = SolarEyes(te_api, sw_api, se_settings, logging)
     except errors.Error as error:
         logging.error(error)
     else:
